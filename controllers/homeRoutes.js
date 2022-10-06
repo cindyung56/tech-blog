a6c3ddb34558bf73c24d470c1f76d650c5c20cf2
cindy <cindyung56@gmail.com>
2022-10-06 15:39:41 -0700

    added comment model, created navbar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,30 +1,28 @@
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    // try{
-    //     const postData = await Post.findAll({
-    //         include: [{model: User, attributes: ['username']}]
-    //     });
-    //     const posts = postData.map((post) => {
-    //         return post.get({plain:true});
-    //     });
-
-    //     res.render('homepage', {
-    //         posts,
-    //         logged_in: req.session.logged_in,
-    //     });
-
-    // } catch (err){
-    //     res.status(500).json(err);
-    // }
-
     try{
-        res.render('homepage');
+        const postData = await Post.findAll({
+            include: [{model: User, attributes: ['username']}]
+        });
+        const posts = postData.map((post) => post.get({plain:true}));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+
     } catch (err){
         res.status(500).json(err);
     }
+
+    // try{
+    //     res.render('homepage');
+    // } catch (err){
+    //     res.status(500).json(err);
+    // }
 })
 
 
