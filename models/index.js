18b3b34c74fe06449fc72ff30fb607302023b471
cindy <cindyung56@gmail.com>
2022-10-05 17:27:22 -0700

    added styling and models

--- models/index.js
+++ models/index.js
@@ -2,3 +2,16 @@
 // user can have multiple posts
 // post can have multiple comments
 // user can also have multiple comments
+
+const User = require('./User');
+const Post = require('./Post');
+// const Comment = require('./Comment');
+
+
+
+
+
+
+
+
+module.exports = { User, Post };
